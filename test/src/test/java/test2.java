b7973da525d471e4eedaae233b6494afcc19eac7
zgc <874587675@qq.com>
2024-09-28 16:40:56 +0800

    1

--- test/src/test/java/test2.java
+++ test/src/test/java/test2.java
@@ -9,8 +9,6 @@
 public class test2 {
     public static void main(String[] args) {
         System.out.println(123);
-        System.out.println(456);
-        System.out.println(1789);
-        System.out.println(12);
+        
     }
 }
